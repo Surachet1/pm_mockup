c4bb34cd927367760513076dddae4fdf833c10cd
root <root@ip-172-31-6-186.ap-southeast-1.compute.internal>
2018-04-02 03:21:39 +0000

    First initial

---
+++ pm_crm/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# © <YEAR(S)> <AUTHOR(S)>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Trinity Roots :: PM - CRM",
+    "summary": "CRM",
+    "version": "10.0.1.0.0",
+    "category": "CRM",
+    "description": """
+
+This module for CRM.
+===========================================================
+
+
+    """,
+    "website": "http://www.trinityroots.co.th/",
+    "author": "Trinity Roots",
+    "license": "AGPL-3",
+    "application": True,
+    "installable": True,
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        'base',
+        'crm',
+    ],
+    'data': [
+    ],
+}
